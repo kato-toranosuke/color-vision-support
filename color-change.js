c0a776f96dd39af4a26d728a85a4a9c5adb323cb
Kato Toranosuke <toranosuke11041999@gmail.com>
2020-12-09 14:15:25 +0900

    修正、分類できる色を増やす

--- color-change.js
+++ color-change.js
@@ -1,14 +1,13 @@
 // 色を近似する
 // h: 0~360, s: 0~100, v:0~100
 const sample_colors = [
-	{ name: 'black', color: { h: -1, s: 100, v: 100 } },
 	{ name: 'red', color: { h: 27, s: 100, v: 100 } },
 	{ name: 'orange', color: { h: 41, s: 100, v: 100 } },
 	{ name: 'yellow', color: { h: 56, s: 100, v: 100 } },
 	{ name: 'green', color: { h: 164, s: 100, v: 100 } },
 	{ name: 'skyblue', color: { h: 200, s: 100, v: 100 } },
 	{ name: 'blue', color: { h: 230, s: 100, v: 100 } },
-	{ name: 'purple', color: { h: 326, s: 100, v: 100 } },
+	{ name: 'pink', color: { h: 326, s: 100, v: 100 } },
 ];
 
 function main() {
@@ -46,14 +45,16 @@ function children_bc_change(element, bc) {
 	}
 }
 
+async function log_element(element) {
+	console.log(element);
+}
 
-function change_color(element) {
+async function get_element_color(element) {
 	let css = getComputedStyle(element, null);
-	// console.log(css);
-	let c = css.getPropertyValue("color");
+	let rgb_color = css.getPropertyValue("color");
 
 	let reg = /(?<=rgb\().*(?=\))/;
-	let result = c.match(reg);
+	let result = rgb_color.match(reg);
 	let result2 = result[0].split(',');
 
 	let rgb = [];
@@ -64,7 +65,31 @@ function change_color(element) {
 	var new_rgb = classify_colors(rgb);
 
 	//return rgb;
-	// console.log(c);
+	const new_rgb_str = `rgb(${new_rgb[0]}, ${new_rgb[1]}, ${new_rgb[2]})`;
+	element.style.color = new_rgb_str;
+}
+
+async function change_color(element) {
+	console.log(element);
+	// await log_element(element);
+	// await get_element_color(element);
+
+	let css = getComputedStyle(element, null);
+	let rgb_color = css.getPropertyValue("color");
+	console.log(rgb_color);
+
+	let reg = /(?<=rgb\().*(?=\))/;
+	let result = rgb_color.match(reg);
+	let result2 = result[0].split(',');
+
+	let rgb = [];
+	rgb[0] = Number(result2[0]);
+	rgb[1] = Number(result2[1]);
+	rgb[2] = Number(result2[2]);
+
+	var new_rgb = classify_colors(rgb);
+
+	//return rgb;
 	const new_rgb_str = `rgb(${new_rgb[0]}, ${new_rgb[1]}, ${new_rgb[2]})`;
 	element.style.color = new_rgb_str;
 
@@ -86,7 +111,7 @@ function background2grey(element) {
 	}
 	else {
 		let gs = (rgb[0]+rgb[1]+rgb[2]) / 3;
-		gs = parseInt(gs/3 + 170);  // ここでグレースケールの濃淡の調整
+		gs = parseInt(gs);  // ここでグレースケールの濃淡の調整
 		bc = "rgb(" + gs + "," + gs + "," + gs + ")";
 		element.style.backgroundColor = bc;
 		console.log(bc);
@@ -110,46 +135,56 @@ function bc2rgb(bc) {
 
 function classify_colors(rgb) {
 	var hsv = rgb2hsv(rgb);
+	// h = 0~360, s,v=0~1
 	var hue = hsv[0];
+	var sat = hsv[1];
 
-	if(hue < 10){
-		// black
-		hsv[0] = sample_colors[0].color.h;
-		hsv[1] = sample_colors[0].color.s;
-		hsv[2] = sample_colors[0].color.v;
-	}else if(hue < 35){
-		// red
-		hsv[0] = sample_colors[1].color.h;
-		hsv[1] = sample_colors[1].color.s;
-		hsv[2] = sample_colors[1].color.v;
-	}else if(hue < 50){
-		// orange
-		hsv[0] = sample_colors[2].color.h;
-		hsv[1] = sample_colors[2].color.s;
-		hsv[2] = sample_colors[2].color.v;
-	}else{
-		// purple
-		hsv[0] = sample_colors[7].color.h;
-		hsv[1] = sample_colors[7].color.s;
-		hsv[2] = sample_colors[7].color.v;
+	// 有彩色の場合
+	if(sat != 0){
+		if(hue < 34){
+			// red
+			hsv[0] = sample_colors[0].color.h;
+			hsv[1] = sample_colors[0].color.s;
+			hsv[2] = sample_colors[0].color.v;
+		}else if(hue < 48){
+			// orange
+			hsv[0] = sample_colors[1].color.h;
+			hsv[1] = sample_colors[1].color.s;
+			hsv[2] = sample_colors[1].color.v;
+		}else if(hue < 110){
+			// yellow
+			hsv[0] = sample_colors[2].color.h;
+			hsv[1] = sample_colors[2].color.s;
+			hsv[2] = sample_colors[2].color.v;
+		}else if(hue < 182){
+			// green
+			hsv[0] = sample_colors[3].color.h;
+			hsv[1] = sample_colors[3].color.s;
+			hsv[2] = sample_colors[3].color.v;
+		}else if(hue < 215){
+			// skyblue
+			hsv[0] = sample_colors[4].color.h;
+			hsv[1] = sample_colors[4].color.s;
+			hsv[2] = sample_colors[4].color.v;
+		}else if(hue < 278){
+			// blue
+			hsv[0] = sample_colors[5].color.h;
+			hsv[1] = sample_colors[5].color.s;
+			hsv[2] = sample_colors[5].color.v;
+		}else{
+			// pink
+			hsv[0] = sample_colors[6].color.h;
+			hsv[1] = sample_colors[6].color.s;
+			hsv[2] = sample_colors[6].color.v;
+		}
 	}
 
 	hsv[1] = hsv[1] / 100;
 	hsv[2] = hsv[2] / 100;
 
 	var new_rgb = hsv2rgb(hsv);
+	// r,g,b = 0~255
 	return new_rgb;
-}
-
-// 要素の色を取得する
-function get_element_color(node) {
-	let rgb = { r: -1, g: -1, b: -1 };
-
-	var body = document.getElementsByTagName('body')[0];
-	children_color_change(body);
-
-
-	return rgb;
 }
 
 function hsv2rgb (hsv) {
